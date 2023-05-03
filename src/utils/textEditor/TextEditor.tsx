45435f80fbf0c3c7b91679851fb61a0bb3e31fef
Hikaru Terazono (3c1u) <3c1u@tohkani.com>
2023-05-03 21:42:28 +0900

    restyle text editor

--- src/utils/textEditor/TextEditor.tsx
+++ src/utils/textEditor/TextEditor.tsx
@@ -1,37 +1,5 @@
 // https://github.com/adazzle/react-data-grid/blob/main/src/editors/textEditor.tsx
-import { css } from '@linaria/core'
 import { EditorProps } from 'react-data-grid'
-
-const textEditorInternalClassname = css`
-  @layer rdg.TextEditor {
-    appearance: none;
-
-    box-sizing: border-box;
-    inline-size: 100%;
-    block-size: 100%;
-    padding-block: 0;
-    padding-inline: 6px;
-    border: 2px solid #ccc;
-    vertical-align: top;
-    color: var(--rdg-color);
-    background-color: var(--rdg-background-color);
-
-    font-family: inherit;
-    font-size: var(--rdg-font-size);
-
-    &:focus {
-      border-color: var(--rdg-selection-color);
-      outline: none;
-    }
-
-    &::placeholder {
-      color: #999;
-      opacity: 1;
-    }
-  }
-`
-
-export const textEditorClassname = `rdg-text-editor ${textEditorInternalClassname}`
 
 function autoFocusAndSelect(input: HTMLInputElement | null) {
   input?.focus()
@@ -46,7 +14,7 @@ export const TextEditor = <TRow, TSummaryRow>({
 }: EditorProps<TRow, TSummaryRow>) => {
   return (
     <input
-      className={textEditorClassname}
+      className='w-full px-2'
       ref={autoFocusAndSelect}
       value={row[column.key as keyof TRow] as unknown as string}
       onChange={event =>
